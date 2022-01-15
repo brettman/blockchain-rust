4f960b64cbb9179ce60a436ea8561fa3a4ef1f58
brettman <brettman@gmail.com>
2022-01-15 20:18:04 +0200

    added a verify function with 4 steps; 1) index matches actual index 2) difficulty is met 3) time increases 4) prev_block_hash matches actual prev_block_hash

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use blockchainlib::*;
 
 fn main() {
     let difficulty = 0x0000ffffffffffffffffffffffffffff;
-    let mut block = Block::new(0, 0, vec![0;32], 0, "Genesis block!".to_owned(), difficulty);
+    let mut block = Block::new(0, now(), vec![0;32], 0, "Genesis block!".to_owned(), difficulty);
     block.mine();
 
     let mut last_hash = block.hash.clone();
@@ -12,13 +12,18 @@ fn main() {
     let mut blockchain = Blockchain{
         blocks: vec![block], 
     };
+    println!("Verify: {}", &blockchain.verify());
 
     // =10 means 1-10 *inclusive*
     for i in 1..=10{
-        let mut block = Block::new(i, 0, last_hash, 0, "another block".to_owned(), difficulty);
+        let mut block = Block::new(i, now(), last_hash, 0, "another block".to_owned(), difficulty);
         block.mine();
         println!("Minded another block {:?}", &block);
         last_hash = block.hash();
         blockchain.blocks.push(block);
+        println!("Verify: {}", &blockchain.verify());
     }
+
+    blockchain.blocks[3].index = 5;
+    println!("Verify: {}", &blockchain.verify());
 }
