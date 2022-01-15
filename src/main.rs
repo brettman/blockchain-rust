6b40e87e3f569c557cadea48c64a797efe121f3c
brettman <brettman@gmail.com>
2022-01-15 19:50:00 +0200

    added a simple blockchain type and tweaked main to generate a few extra mined blocks;

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use blockchainlib::*;
 
 fn main() {
-    let mut block = Block::new(0, now(), vec![0;32], 0, "Genesis block!".to_owned(), 0x0000ffffffffffffffffffffffffffff);
-    block.hash = block.hash();
-
-    println!("{:?}", &block);
-
+    let difficulty = 0x0000ffffffffffffffffffffffffffff;
+    let mut block = Block::new(0, 0, vec![0;32], 0, "Genesis block!".to_owned(), difficulty);
     block.mine();
 
-    println!("{:?}", &block);
+    let mut last_hash = block.hash.clone();
+
+    println!("Mined genesis block {:?}", &block);
+
+    let mut blockchain = Blockchain{
+        blocks: vec![block], 
+    };
+
+    // =10 means 1-10 *inclusive*
+    for i in 1..=10{
+        let mut block = Block::new(i, 0, last_hash, 0, "another block".to_owned(), difficulty);
+        block.mine();
+        println!("Minded another block {:?}", &block);
+        last_hash = block.hash();
+        blockchain.blocks.push(block);
+    }
 }
